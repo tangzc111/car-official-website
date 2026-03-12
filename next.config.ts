f8cb7ab1cc261f02d8f1af3adbf0b70aa3ec5b60
tangzc111 <454823887@qq.com>
2026-03-13 01:13:58 +0800

    feat: 打包静态页面

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
